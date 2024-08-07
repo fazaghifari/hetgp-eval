52eced3fd3f982dd64222d16da7208365a00b257
Ghifari Adam Faza <fazaghifari@MSI.localdomain>
2024-08-07 16:14:28 +0200

    update latest

--- src/mlhgp.py
+++ src/mlhgp.py
@@ -109,3 +109,73 @@ class MLHGP():
                 
             
         return result
+
+class HomGP():
+    """Wrapper for homoscedastic GP so everything are in the same API style
+    """
+
+    def __init__(self, model=None, model_noise=None, 
+                 max_iter=5, noise_sample_size=150):
+        self.model = model
+        self.model_noise = model_noise
+        self.max_iter = max_iter
+        self.noise_sample_size = noise_sample_size
+        self.variance_est = None
+    
+    def fit(self,X,y):
+        """Fit the model
+
+        Args:
+            X (np.array): nxm matrix, n is the number of sample, m is the number of dimension
+            y (np.array): nx1 matrix
+        """
+
+        # Get the dimension of the features
+        self.dim = X.shape[1] 
+
+        ## Step 1
+        # Fit standard homoscedastic GP on the training dataset
+        self.model.fit(X, y)
+        mean_pred, std_pred =  self.model.predict(X, return_std=True)
+    
+    def predict(self, X, return_std=None):
+        """
+        Make a prediction for X input. Standard deviation can be separated to two types: aleatoric (inherent noise from the data)
+        and epistemic (uncertainty of the model itself).
+
+
+        Params:
+            X:  input data for which to make a prediction
+            return_std: if True, returns mean and the full std (aleatoric+epistemic)
+            return_al_std: if True, returns mean and aleatoric std
+            return_ep_std: if True, returns mean and epistemic std
+
+        """
+        if return_std is None:
+            result = self.model.predict(X)
+        else:
+            assert_msg = "return_std options are ['total','epistemic','aleatoric','multi']"
+            assert return_std.lower() in ['total','epistemic','aleatoric','multi'], assert_msg
+
+            mean, std_ep = self.model.predict(X, return_std=True)
+            st = np.ones(shape=std_ep.shape) 
+            if return_std.lower()=="epistemic":
+                # Epistemic std
+                std = std_ep
+            elif return_std.lower()=="aleatoric":
+                # Aleatoric std
+                std = np.sqrt(np.exp(self.model.kernel_.theta[-1])) * st
+            elif return_std.lower()=="total":
+                # Full std (epistemic + aleatoric)
+                var_ep = std_ep**2
+                var_al = np.exp(self.model.kernel_.theta[-1]) * st
+                std=np.sqrt(var_ep+var_al)
+            else:
+                # Return aleatoric and epistemic separately
+                std_al = np.sqrt(np.exp(self.model.kernel_.theta[-1])) * st
+                std = [std_al, std_ep]
+            
+            result = mean, std
+                
+            
+        return result
